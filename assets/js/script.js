0842ec05791d4a3fa884413d25ab634603061bb6
Thiago Nascimento <nascimentoliveira@gmail.com>
2022-08-22 14:24:32 -0300

    Layout mobile revisado, tempos de animação revisados, relógio implementado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,27 @@
-let cardsDealt = [];
-let previousSelected;
-let nCardSelected;
-let clicks;
-let found;
-let time;
-let idSetInterval;
+let cardsDealt = [];  //drawn cards
+let previousSelected; //face-up card (first of pair)
+let nCardSelected;    //how many cards are face up
+let moves;            //number of movements
+let found;            //number of cards found 
+let time;             //elapsed time
+let idSetInterval;    //generated setInterval id
 
-function updateDisplay() {
-    time += 0.1;
-    const turns = document.querySelector(".clock-score :nth-child(1) p")
-    const timer = document.querySelector(".clock-score :nth-child(3) p");
-    turns.innerHTML = clicks;
-    timer.innerHTML = time.toFixed(1);
+const turningTime = 800;
+const observationTime = 1000;
+const animationTime = 1000;
+const watchInterval = 0.1;
+const decimalPlace = 1;
+const secondXMiliseconds = 1000;
+
+function updateDisplay(score, timer) {
+
+    time += watchInterval;
+    score.innerHTML = moves;
+    timer.innerHTML = `${time.toFixed(decimalPlace)} s`;
   }
 
-function checkParity(index1, index2){
+function checkParity(index1, index2) {
+
     if (cardsDealt[index1] === cardsDealt[index2]) {
         return true;
     } else {
@@ -23,34 +30,42 @@ function checkParity(index1, index2){
 }
 
 function ocultElement(element) {
+
     element.querySelector(":nth-child(1)").classList.toggle("ocult");
     element.querySelector(":nth-child(2)").classList.toggle("ocult");
 }
 
 function turn(figure) {
+
     figure.classList.toggle("upturned");
-    setTimeout(ocultElement, 500, figure);
+    setTimeout(ocultElement, turningTime/2, figure);
 }
 
-function unblock(){
+function unblock() {
+
     document.querySelector(".website > div").classList.remove("block-on");
 }
 
 function resolved(card1, card2) {
+
     card1.classList.add("resolved");
     card2.classList.add("resolved");
 }
 
 function checkEnd() {
+
     const website = document.querySelector(".website")
     let answer;
+
     found += 2;
-    
+
     if (found === cardsDealt.length) {
         clearInterval(idSetInterval);
-        alert(`Você ganhou em ${clicks} jogadas!`);
+        alert(`Você ganhou com ${moves} jogadas em ${time.toFixed(decimalPlace)} segundos!`);
+
         do {
             answer = prompt("Deseja jogar novamente?");
+
             if (answer === "sim") {
                 init();
                 break;
@@ -60,31 +75,33 @@ function checkEnd() {
                 website.setAttribute("onclick","init()");
                 break;
             } else {
-                alert("Esta pergunta aceita a 'sim' ou 'não' como resposta!");
+                alert("Esta pergunta aceita 'sim' ou 'não' como resposta!");
             }
+
        } while (true);
     }
 }
 
 function select(figure, index) {
+
     nCardSelected++;
-    clicks++;
+    moves++;
+
     figure.removeAttribute("onclick");
     turn(figure); 
     
     if (nCardSelected == 2){
         document.querySelector(".website > div").classList.add("block-on");
         if (checkParity(previousSelectedIndex, index)) {
-            setTimeout(checkEnd, 1000);
-            setTimeout(resolved, 500, figure, previousSelectedFigure);
-            setTimeout(unblock, 500);
-
+            setTimeout(checkEnd, turningTime + turningTime/2);
+            setTimeout(resolved, turningTime, figure, previousSelectedFigure);
+            setTimeout(unblock, turningTime);
         } else {
-            setTimeout(turn, 1500, previousSelectedFigure);
-            setTimeout(turn, 1500, figure);
+            setTimeout(turn, observationTime + turningTime, previousSelectedFigure);
+            setTimeout(turn, observationTime + turningTime, figure);
             previousSelectedFigure.setAttribute("onclick", `select(this, ${previousSelectedIndex})`);
             figure.setAttribute("onclick", `select(this, ${index})`);
-            setTimeout(unblock, 2000);
+            setTimeout(unblock, observationTime + turningTime);
         }
         nCardSelected = 0;
 
@@ -94,66 +111,79 @@ function select(figure, index) {
     } 
 }
 
-function removeStart() {
-    document.querySelectorAll('section .start').forEach(n => n.classList.remove('start'));
+function removeStart(cards) {
+
+    for (let i=0; i<cards.length; i++){
+        cards[i].classList.remove('start');
+    }
 }
 
+//https://stackoverflow.com/a/2450976/15506353
 function shuffle(array) {
+
     let currentIndex = array.length,  randomIndex;
-  
+
     while (currentIndex != 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
       [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
     }
-  
+
     return array;
 }
 
 function createCards(nCards) {
+
     const section = document.querySelector("main section");
-    let cards = ["bobross", "explody", "fiesta", "metal", "revertit", "triplets", "unicorn"];
+    const cards = ["bobross", "explody", "fiesta", "metal", "revertit", "triplets", "unicorn"];
 
-    cards = shuffle(cards);
-    cards = cards.slice(0, nCards/2);
-    cards = cards.concat(cards);
     cardsDealt = shuffle(cards);
-
+    cardsDealt = cardsDealt.slice(0, nCards/2);
+    cardsDealt = cardsDealt.concat(cardsDealt);
+    cardsDealt = shuffle(cardsDealt);
     section.innerHTML = "";
 
-    for(let i=1; i<=nCards; i++){
-        section.innerHTML += `<figure class="start" onclick="select(this, ${i-1})">
-                                  <img src="./assets/images/front.png" alt="${i}-th carta"/>
-                                  <img class="ocult" src="./assets/images/${cardsDealt[i-1]}parrot.gif" alt="${i}-th carta, imagem ${cardsDealt[i-1]}parrot"/>
+    for(let i=0; i<nCards; i++){
+        section.innerHTML += `<figure class="start" onclick="select(this, ${i})">
+                                  <img src="./assets/images/front.png" alt="${i+1}-th carta"/>
+                                  <img class="ocult" src="./assets/images/${cardsDealt[i]}parrot.gif" alt="${i+1}-th carta, imagem ${cardsDealt[i]}parrot"/>
                               </figure>`;
     }
 
-    setTimeout(removeStart, 1000);
+    setTimeout(removeStart, animationTime, section.querySelectorAll('.start'));
 }
 
 function howManyCards() {
+
+    const minNCards = 4;
+    const maxNCards = 14;
     let nCards;
 
     do {
         nCards = Number(prompt("Com quantas cartas deseja jogar?"));
-        if ((nCards >= 4 && nCards <= 14 && nCards % 2 === 0) || nCards ===  0) {
+
+        if ((nCards >= minNCards && nCards <= maxNCards && nCards % 2 === 0) || nCards ===  0) {
             break;
         } else {
             alert("Somente números pares entre 4 e 14 (inclusive)!");
         }
+
     } while (true);
 
     return nCards;
 }
 
 function init() {
+
+    cardsDealt = [];
     nCardSelected = 0;
-    clicks = 0;
+    moves = 0;
     found = 0;
     time = 0;
+
     const website = document.querySelector(".website");
     const nCards = howManyCards();
-    
+
     if (nCards === 0) {
         website.setAttribute("onclick","init()");
         website.querySelector("header p").innerHTML = "CLIQUE PARA RECOMEÇAR!";
@@ -161,8 +191,11 @@ function init() {
         website.removeAttribute("onclick");
         createCards(nCards);
         website.classList.remove("start");
-        idSetInterval = setInterval(updateDisplay, 100);
+        idSetInterval = setInterval(updateDisplay, 
+                                    watchInterval*secondXMiliseconds, 
+                                    website.querySelector(".clock-score :nth-child(1) p"), 
+                                    website.querySelector(".clock-score :nth-child(3) p"));
     }
 }
 
-setTimeout(init, 1000);
+setTimeout(init, animationTime);
